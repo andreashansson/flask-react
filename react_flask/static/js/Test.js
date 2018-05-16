a795e416dfc5f167334e24b93610c7706b322e31
Andreas Hansson <andenwee@gmail.com>
2018-05-16 23:38:30 +0200

    Changed Test module to figure out delay from api

--- react_flask/static/js/Test.js
+++ react_flask/static/js/Test.js
@@ -1,7 +1,3 @@
 export var test = function() {
-  fetch("/test/andreas/", {method: "GET"}).then(function(response) {
-    return response.json();
-  }).then(function(result) {
-    console.log(result);
-  });
+	console.log("CLICK");
 }
