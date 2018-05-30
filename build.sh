8af73f9cfa6abd2946dc89ad6297f55b0a89e2de
andreas hansson <andreas.hansson@icomera.com>
2018-05-30 08:37:16 +0200

    Builds react app in build script

--- build.sh
+++ build.sh
@@ -3,7 +3,12 @@
 rm -rf bin
 mkdir bin
 
-tar --create --gzip --verbose --file react_flask.tar.gz --exclude __pycache__ --exclude config_example.json -C react_flask .
+cd react_flask/static
+npm run build
+cd ..
+cd ..
+
+tar --create --gzip --verbose --file react_flask.tar.gz --exclude __pycache__ --exclude static/node_modules --exclude static/images --exclude static/sass --exclude config_example.json -C react_flask .
 mv -f react_flask.tar.gz bin/
 
 set -e
